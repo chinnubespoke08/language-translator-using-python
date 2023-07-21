8626bee65ca918d1d1d57a2ca8b3141c8cd51218
Priyatham Ch <54296329+chinnubespoke08@users.noreply.github.com>
2023-07-21 15:14:12 +0530

    language translator from english to french and vice-versa

---
+++ server.py
@@ -0,0 +1,27 @@
+import machinetranslation
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    translatedFrench = machinetranslation.translator.englishToFrench(textToTranslate)
+    return translatedFrench
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    translatedEnglish = machinetranslation.translator.frenchToEnglish(textToTranslate)
+    return translatedEnglish
+
+@app.route("/")
+def renderIndexPage():
+    # Write the code to render template
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
